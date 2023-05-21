71409f5b3d0bff55f760cf231c124ec1a6697b72
sasasan03 <sako.hiro62@gmail.com>
2023-05-21 13:44:07 +0900

    sliderとLabelを追加

---
+++ Kadai8-UIkit/OrangeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  OrangeViewController.swift
+//  Kadai8-UIkit
+//
+//  Created by sako0602 on 2023/05/21.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
