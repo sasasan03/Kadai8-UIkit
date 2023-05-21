b7e10ec817dd82dddd091594ba3ff8128fb38de7
sasasan03 <sako.hiro62@gmail.com>
2023-05-21 12:41:00 +0900

    first commit

---
+++ Kadai8-UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai8-UIkit
+//
+//  Created by sako0602 on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
